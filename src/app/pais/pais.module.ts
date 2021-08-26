9c07f4c8a98fc33ae3c488452b501a6a169ea47a
Walter <waltervillatoro2@gmail.com>
2021-08-26 17:23:26 -0600

    ok 26-08-21

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     PorCapitalComponent,
@@ -12,7 +15,8 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     VerPaisComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, FormsModule, HttpClientModule
+
   ],
   exports: [
     PorCapitalComponent,
